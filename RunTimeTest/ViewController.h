2536cf1647b3bb51207b7a99c1d75f0913307c34
guzhiyang <guzhiyang@localhost>
2016-02-29 16:44:57 +0800

    Initial Commit

---
+++ RunTimeTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RunTimeTest
+//
+//  Created by guzhiyang on 16/2/29.
+//  Copyright © 2016年 guzhiyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ RunTimeTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RunTimeTest
+//
+//  Created by guzhiyang on 16/2/29.
+//  Copyright © 2016年 guzhiyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
